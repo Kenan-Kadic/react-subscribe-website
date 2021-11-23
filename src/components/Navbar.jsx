f0e0c5ef88c9cabf3a7e54942a53950c0711d508
Kenan Kadic <ken.kadic@outlook.com>
2021-11-23 16:35:51 -0600

    navbar page added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    height: 50px;
+`
+const Wrapper = styled.div`
+    padding: 10px 20px;
+`
+
+const Navbar = () => {
+    return (
+        <Container>
+            navbar
+        </Container>
+    )
+}
+
+export default Navbar
