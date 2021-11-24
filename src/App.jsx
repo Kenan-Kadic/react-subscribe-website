4e2c567a09fc99f63eed1408821a5ca1feb034b0
Kenan Kadic <ken.kadic@outlook.com>
2021-11-24 10:50:21 -0600

    update Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 import Navbar from "./components/Navbar";
+import Intro from "./components/Intro";
 
 const Container = styled.div`
     height: 100vh;    
 `
 
+
+
 const App = () => {
-  return <Container><Navbar/></Container>;
+  return <Container><Navbar/><Intro/></Container>;
 };
 
 
