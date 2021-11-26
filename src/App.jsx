e1617e020e3bbd12c2bca597cf98b2423cce076f
Kenan Kadic <ken.kadic@outlook.com>
2021-11-26 09:40:35 -0600

    Feature.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Navbar from "./components/Navbar";
 import Intro from "./components/Intro";
+import Feature from "./components/Feature";
 
 const Container = styled.div`
-    height: 100vh; 
-    overflow: hidden;   
-`
-const IntroShape = styled.div`
+   height: 100vh; 
+   overflow: hidden;
+   position: relative;   
+`;
+const Shape = css`
   width: 100%;
   height 100%;
   position: absolute;
   top: 0;
   left 0;
   z-index: -1;
+
+`;
+
+const IntroShape = styled.div`
+  ${Shape}
   clip-path: polygon(67% 0, 100% 0%, 100% 100%, 55% 100%);
   background-color: #cce6ff;
 `;
 
+const FeatureShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0 0, 55% 0%, 100% 100%, 55% 100%);
+  background-color: lightgreen;
+`;
 
 const App = () => {
   return (
@@ -25,6 +37,10 @@ const App = () => {
     <Navbar />
       <Intro />
       <IntroShape />
+  </Container>
+  <Container>
+    <Feature/>
+    <FeatureShape />
   </Container>
   </>
   );
