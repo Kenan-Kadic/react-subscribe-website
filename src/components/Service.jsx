f7c5e7ce914e8fc1d4e6108bcef8f0fce4402362
Kenan Kadic <ken.kadic@outlook.com>
2021-12-02 14:52:47 -0600

    small updates to font

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -128,8 +128,8 @@ const Service = () => {
           <Desc>
             We provide digital experience services to startups and small
             businesses to looking for a partner of their digital media, design &
-            development, lead generation and communications requirents. We work
-            with you, not for you. Although we have a great resources
+            development, lead generation and communication requirements. We work
+            with you, not for you. We have a great resources
           </Desc>
           <CardContainer>
             <MiniCard cardText="Search Designs"/>
